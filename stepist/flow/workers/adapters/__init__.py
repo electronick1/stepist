a06b9ad28b94e238da5783a38120daf2f44d4b2c
oleg <oleg.ivye@gmail.com>
2019-03-13 23:07:14 +0300

    Remove adapters auto initialization

--- stepist/flow/workers/adapters/__init__.py
+++
@@ -1,3 +0,0 @@
-from stepist.flow.workers.adapters.simple_queue import SimpleQueueAdapter
-from stepist.flow.workers.adapters.rm_queue import RQAdapter
-from stepist.flow.workers.adapters.sqs_queue import SQSAdapter
