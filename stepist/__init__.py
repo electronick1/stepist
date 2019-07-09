08989fcd7770fe96e06bb58c675385138e54acf8
oleg <oleg.ivye@gmail.com>
2019-07-09 16:50:32 +0300

    Exclude services imports

--- stepist/__init__.py
+++ stepist/__init__.py
@@ -1,7 +1,10 @@
 from .flow import *
 from .flow.steps import *
 from .app import App
-from .flow.workers.adapters.rm_queue import RQAdapter
-from .flow.workers.adapters.sqs_queue import SQSAdapter
+try:
+    from .flow.workers.adapters.rm_queue import RQAdapter
+    from .flow.workers.adapters.sqs_queue import SQSAdapter
+except ModuleNotFoundError:
+    pass
 from .flow.workers.adapters.simple_queue import SimpleQueueAdapter as RedisAdapter
 
